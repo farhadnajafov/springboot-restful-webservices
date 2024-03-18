72c8d5e8329c2f1708ab8c0040e32494a078d6b8
Farhad I. Nacafov <Farhad.Nacafov@kapitalbank.az>
2024-03-18 17:46:59 +0400

    swagger configuration

--- src/main/java/com/example/springboot/controller/UserController.java
+++ src/main/java/com/example/springboot/controller/UserController.java
@@ -5,6 +5,9 @@ import com.example.springboot.entity.User;
 import com.example.springboot.exception.ErrorDetails;
 import com.example.springboot.exception.ResourceNotFoundException;
 import com.example.springboot.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +18,10 @@ import org.springframework.web.context.request.WebRequest;
 import java.time.LocalDateTime;
 import java.util.List;
 
-
+@Tag(
+        name = "CRUD REST APIs for User Resource",
+        description = "CRUD REST APIs - Create User, Update User, Get User, Get All Users, Delete User"
+)
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
@@ -26,12 +32,31 @@ public class UserController {
         this.userService = userService;
     }
 
+    @Operation(
+            summary = "Create User Rest API",
+            description = "Create User REST API is used to save user in a database"
+    )
+    @ApiResponse(
+            responseCode = "201",
+            description = "HTTP Status 201 CREATED"
+    )
+
     @PostMapping("/createUser")
     public ResponseEntity<UserDto> createUser(@Valid @RequestBody UserDto userDto) {
         UserDto savedUserDto = userService.createUser(userDto);
 
         return new ResponseEntity<>(savedUserDto, HttpStatus.CREATED);
     }
+
+
+    @Operation(
+            summary = "Get All Users  REST API",
+            description = "Get All Users REST API is used to get all the users  from the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
 
 
     @GetMapping("/getAllUser")
@@ -42,11 +67,31 @@ public class UserController {
 
     }
 
+
+    @Operation(
+            summary = "Get User By ID REST API",
+            description = "Get User By ID REST API is used to get a single user from database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
+
     @GetMapping("getUserById/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable("id") Long id) {
         UserDto getUserById = userService.getUserById(id);
         return new ResponseEntity<>(getUserById, HttpStatus.OK);
     }
+
+
+    @Operation(
+            summary = "Update User  REST API",
+            description = "Update User REST API is used to update a particular user in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
 
     @PutMapping("/updateUser/{id}")
     public ResponseEntity<UserDto> updateUser(@PathVariable("id") Long userId,
@@ -55,6 +100,15 @@ public class UserController {
         UserDto updateUser = userService.updateUser(userDto);
         return new ResponseEntity<>(updateUser, HttpStatus.OK);
     }
+
+    @Operation(
+            summary = "Delete User  REST API",
+            description = "Delete User REST API is used to delete a particular user in the database"
+    )
+    @ApiResponse(
+            responseCode = "200",
+            description = "HTTP Status 200 SUCCESS"
+    )
 
     @DeleteMapping("/deleteUser/{id}")
     public ResponseEntity<String> deleteUser(@PathVariable("id") Long userId) {
--- src/main/java/com/example/springboot/dto/UserDto.java
+++ src/main/java/com/example/springboot/dto/UserDto.java
@@ -1,12 +1,15 @@
 package com.example.springboot.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
+@Schema(
+        description = "UserDto Model Information"
+)
 @Getter
 @Setter
 @NoArgsConstructor
@@ -14,10 +17,16 @@ import lombok.Setter;
 public class UserDto {
 
     private Long id;
+
+    @Schema(description = "User First Name")
     @NotEmpty(message = "User first name should not be null or empty")
     private String firstName;
+
+    @Schema(description = "User Last Name")
     @NotEmpty(message = "User last name should not be null or empty")
     private String lastName;
+
+    @Schema(description = "User Email Address")
     @NotEmpty(message = "User email should not be null or empty")
     @Email(message = "Email address should be valid")
     private String email;
